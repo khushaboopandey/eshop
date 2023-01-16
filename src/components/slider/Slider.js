48c21bd456bb80cf9a20caeb86c80cf3348812a8
khushaboopandey <pandeykhushaboo@gmail.com>
2023-01-16 15:53:23 +0530

    added prev and next feature

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
+import { sliderData } from "./slider_data";
+import "./Slider.scss";
 
 const Slider = () => {
-  return <div>dcdc</div>;
+  const [currentSlide, setcurrentSlide] = useState(0);
+  const slideLength = sliderData.length;
+  const prevSlide = () => {
+    setcurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1);
+  };
+  const nextSlide = () => {
+    setcurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
+  };
+
+  return (
+    <div className="slider">
+      <AiOutlineArrowLeft className="arrow prev" onClick={prevSlide} />
+      <AiOutlineArrowRight className="arrow next" onClick={nextSlide} />
+      {sliderData.map((slide, index) => {
+        const { image, heading, desc } = slide;
+        return (
+          <div
+            key={index}
+            className={index === currentSlide ? "slide current" : "slide"}
+          >
+            {index === currentSlide && (
+              <>
+                <img src={image} alt="slide" />
+                <div className="content">
+                  <h2>{heading}</h2>
+                  <p>{desc}</p>
+                  <hr />
+                  <a href="#product" className="--btn --btn-primary">
+                    Shop Now
+                  </a>
+                </div>
+              </>
+            )}
+            slide
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Slider;
